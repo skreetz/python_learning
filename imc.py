d30e486a141b5f8064b3cacf4fe02e02d6e5251a
Lucas Maia <41240622+skreetz@users.noreply.github.com>
2025-07-30 10:13:03 -0300

    modified:   imc.py

--- imc.py
+++ imc.py
@@ -1,1 +1,8 @@
-peso = input("Digite o ")
+peso = input("Digite o seu peso em kg: ")
+altura = input("Digite a sua altura em centímetros: ")
+
+peso = float(peso)
+altura = float(altura)
+
+imc = peso / altura**2
+print("Seu IMC é:" + str(imc))
