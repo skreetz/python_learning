6d4583d8b74e435d1d8294f219cce659a69a4b00
Lucas Maia <41240622+skreetz@users.noreply.github.com>
2025-07-31 14:32:48 -0300

    new file:   aritmetic.py 	renamed:    variaveis.py -> variables.py

---
+++ aritmetic.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite otro número: "))
+sm = n1 + n2
+print ("A soma vale {}".format(sm)) # usa-se .format com {} (strings formatadas), ou seja, os valores SERÃO inseridos
+print ("A soma vale", sm)
+
+print("'2 elevado a 3 é igual a 8'", 2**3) # ** = potência
+print("'2 dividido por 3 sobra 0'", 2//3) # // = divisão inteira
+print(2%3) # % = resto da divisão ou mod
