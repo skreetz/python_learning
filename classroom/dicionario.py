22735e1328a90b6997a9a5a33500e20d8fb81a26
Lucas Maia <lucasdocarmomaia@gmail.com>
2025-08-12 20:03:17 -0300

    modified:   classroom/dicionario.py

--- classroom/dicionario.py
+++ classroom/dicionario.py
@@ -1,1 +1,5 @@
-# dicionario que amarzena e dps exibe, nome idade e nota final
+import numpy as np
+
+matriz = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("A soma dos elementos é:", matriz)
+print(matriz)
