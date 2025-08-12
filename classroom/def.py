8a714cf24bb41fc4b4401690757ee0fe136c2d5d
Lucas Maia <lucasdocarmomaia@gmail.com>
2025-08-12 20:28:27 -0300

    new file:   classroom/def.py 	modified:   classroom/dicionario.py

---
+++ classroom/def.py
@@ -0,0 +1,9 @@
+# def saudacao():
+#     print("Olá, seja bem-vindo ao nosso programa!")
+# saudacao()
+
+def somar(a,b):
+    return a + b
+
+resultado = somar(5, 3)
+print("Resultado da soma:", resultado)
