59d45832dd82078eda2e781c738e2cef44b4b77b
Lucas Maia <lucasdocarmomaia@gmail.com>
2025-08-12 21:11:37 -0300

    modified:   classroom/def.py

--- classroom/def.py
+++ classroom/def.py
@@ -2,8 +2,20 @@
 #     print("Olá, seja bem-vindo ao nosso programa!")
 # saudacao()
 
-def somar(a,b):
-    return a + b
+# def somar(a,b):
+#     return a + b
 
-resultado = somar(5, 3)
-print("Resultado da soma:", resultado)
+# resultado = somar(5, 3)
+# print("Resultado da soma:", resultado)
+
+def adicao(a,b):
+    return (a + b)
+def subtracao(a,b):
+    return (a - b)
+def multiplicacao (a,b):
+    return (a * b)
+def divisao (a,b):
+    return (a/b)
+
+resultado = subtracao(3, 2)
+print("Resultado da conta: ", resultado)
